692fd43aec5b288e78bcaaeef956231b454ebd1a
rodrigombsoares <rodrigombsoares@gmail.com>
2023-11-24 00:11:25 -0300

    04_primitive_types DONE ✅

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -5,10 +5,9 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // let a = [42; 101];
+    let a = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick ";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
